2d8a31d947d500d66a489c7c33290d8f64d3b169
Brendan Butler <brendan.butler@digital.cabinet-office.gov.uk>
2016-03-23 15:29:54 +0000

    Changes to jenkins.sh for govuk-lint and db creation

--- jenkins.sh
+++ jenkins.sh
@@ -35,23 +35,22 @@ git clean -fdx
 # is master.
 git merge --no-commit origin/master || git merge --abort
 
+export RAILS_ENV=test
+bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment --without development
+
 # Lint changes introduced in this branch, but not for master
 if [[ ${GIT_BRANCH} != "origin/master" ]]; then
   bundle exec govuk-lint-ruby \
-    --diff \
-    --cached \
-    --format html --out rubocop-${GIT_COMMIT}.html \
-    --format clang \
-    app spec lib
+  --diff \
+  --cached \
+  --format html --out rubocop-${GIT_COMMIT}.html \
+  --format clang \
+  app spec lib
 
   bundle exec govuk-lint-sass app
 fi
 
-export RAILS_ENV=test
-bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment --without development
-
-# Uncomment if this app uses a database
-# bundle exec rake db:drop db:create db:schema:load
+bundle exec rake db:drop db:create db:schema:load
 
 if bundle exec rake ${TEST_TASK:-"default"}; then
   github_status success "succeeded on Jenkins"
