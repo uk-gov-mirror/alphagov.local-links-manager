21b1de03ff57749d6c8d09db1d419e856c3d2cca
Murray Steele <muz@h-lame.com>
2016-05-11 14:59:10 +0000

    Allow a Service to say if it is provided_by? an authority
    We do a comparison of the service tier with the authority tier and return
    true or false accordingly.  A blank tier is always false, an 'all' tier is
    always true, other values require the authority tier to match the service
    tier.

--- app/models/service.rb
+++ app/models/service.rb
@@ -3,4 +3,21 @@ class Service < ActiveRecord::Base
   validates :tier, inclusion: { in: %w{all county/unitary district/unitary}, allow_nil: true }
 
   has_many :service_interactions
+
+  def provided_by?(authority)
+    case tier
+    when nil
+      false
+    when 'all'
+      true
+    else
+      tiers.include? authority.tier
+    end
+  end
+
+private
+
+  def tiers
+    tier.split('/')
+  end
 end
--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -13,4 +13,62 @@ RSpec.describe Service, type: :model do
   it { is_expected.to allow_value(nil).for(:tier) }
 
   it { is_expected.to have_many(:service_interactions) }
+
+  describe '#provided_by?' do
+    let(:district) { FactoryGirl.build(:local_authority, tier: 'district') }
+    let(:county) { FactoryGirl.build(:local_authority, tier: 'county') }
+    let(:unitary) { FactoryGirl.build(:local_authority, tier: 'unitary') }
+
+    context 'when the tier is blank' do
+      subject { FactoryGirl.build(:service, tier: nil) }
+      it 'is false for a district council' do
+        expect(subject).not_to be_provided_by(district)
+      end
+      it 'is false for a county council' do
+        expect(subject).not_to be_provided_by(county)
+      end
+      it 'is false for a unitary council' do
+        expect(subject).not_to be_provided_by(unitary)
+      end
+    end
+
+    context 'when the tier is all' do
+      subject { FactoryGirl.build(:service, tier: 'all') }
+      it 'is true for a district council' do
+        expect(subject).to be_provided_by(district)
+      end
+      it 'is true for a county council' do
+        expect(subject).to be_provided_by(county)
+      end
+      it 'is true for a unitary council' do
+        expect(subject).to be_provided_by(unitary)
+      end
+    end
+
+    context 'when the tier is district/unitary' do
+      subject { FactoryGirl.build(:service, tier: 'district/unitary') }
+      it 'is true for a district council' do
+        expect(subject).to be_provided_by(district)
+      end
+      it 'is false for a county council' do
+        expect(subject).not_to be_provided_by(county)
+      end
+      it 'is true for a unitary council' do
+        expect(subject).to be_provided_by(unitary)
+      end
+    end
+
+    context 'when the tier is county/unitary' do
+      subject { FactoryGirl.build(:service, tier: 'county/unitary') }
+      it 'is false for a district council' do
+        expect(subject).not_to be_provided_by(district)
+      end
+      it 'is true for a county council' do
+        expect(subject).to be_provided_by(county)
+      end
+      it 'is true for a unitary council' do
+        expect(subject).to be_provided_by(unitary)
+      end
+    end
+  end
 end
