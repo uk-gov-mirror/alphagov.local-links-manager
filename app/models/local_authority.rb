d4c7fffd4c61fecf1b34021078292d76f6f4935a
Murray Steele <muz@h-lame.com>
2016-05-16 14:15:44 +0000

    Limit services shown on LA page to those for its' tier

--- app/models/local_authority.rb
+++ app/models/local_authority.rb
@@ -4,4 +4,8 @@ class LocalAuthority < ActiveRecord::Base
   validates :homepage_url, format: { with: URI.regexp }, allow_blank: true
   validates :tier, inclusion: { in: %w(county district unitary),
     message: "%{value} is not an allowed tier" }
+
+  def provided_services
+    Service.for_tier(self.tier)
+  end
 end
