5bb682243a276205c1caa94d8695431f3857d90e
Brendan Butler <brendan.butler@digital.cabinet-office.gov.uk>
2016-06-02 13:06:40 +0000

    Add simple link controller test
    Our controller is mostly being exercised via the feature tests.

--- spec/controllers/links_controller_spec.rb
+++ spec/controllers/links_controller_spec.rb
@@ -1,4 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe LinksController, type: :controller do
+  before do
+    @local_authority = FactoryGirl.create(:local_authority, name: 'Angus')
+    @service = FactoryGirl.create(:service, label: 'Service 1', lgsl_code: 1)
+    @interaction = FactoryGirl.create(:interaction, label: 'Interaction 1', lgil_code: 3)
+  end
+
+  describe 'GET edit' do
+    it 'retrieves HTTP success' do
+      login_as_stub_user
+
+      get :edit, local_authority_slug: @local_authority.slug, service_slug: @service.slug, interaction_slug: @interaction.slug
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
