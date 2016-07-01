540832a6e622eec4c0bcdf187c3f672cae9defea
David Basalla <davidbasalla@gmail.com>
2016-07-01 10:52:46 +0100

    Add disabled_service factory
    Make the default Service Factory be enabled, as we most likely will be testing Links for enabled Services more often, thus reducing the extra effort of setting a Service to be enabled for every test.
    We update the one test that uses disabled Services. The second test was not actually invoking the import task, so we update it.

--- spec/factories/services.rb
+++ spec/factories/services.rb
@@ -3,6 +3,10 @@ FactoryGirl.define do
     sequence(:lgsl_code) { |n| n }
     sequence(:label) { |n| "Service Label #{n}" }
     slug { label.parameterize }
+    enabled true
+  end
+
+  factory :disabled_service, parent: :service do
     enabled false
   end
 end
--- spec/lib/local-links-manager/import/enabled_service_checker_spec.rb
+++ spec/lib/local-links-manager/import/enabled_service_checker_spec.rb
@@ -5,9 +5,9 @@ describe LocalLinksManager::Import::EnabledServiceChecker, :csv_importer do
   describe '#enabled_services' do
     let(:csv_downloader) { instance_double CsvDownloader }
     let(:csv_rows) { [{ "LGSL" => 1614 }, { "LGSL" => 13 }] }
-    let!(:service_0) { FactoryGirl.create(:service, lgsl_code: 1614, label: "Bursary Fund Service") }
-    let!(:service_1) { FactoryGirl.create(:service, lgsl_code: 13, label: "Abandoned shopping trolleys") }
-    let!(:service_2) { FactoryGirl.create(:service, lgsl_code: 10, label: "Special educational needs - placement in mainstream school") }
+    let!(:service_0) { FactoryGirl.create(:disabled_service, lgsl_code: 1614, label: "Bursary Fund Service") }
+    let!(:service_1) { FactoryGirl.create(:disabled_service, lgsl_code: 13, label: "Abandoned shopping trolleys") }
+    let!(:service_2) { FactoryGirl.create(:disabled_service, lgsl_code: 10, label: "Special educational needs - placement in mainstream school") }
 
     context 'when the csv is downloaded successfully' do
       before do
@@ -21,6 +21,7 @@ describe LocalLinksManager::Import::EnabledServiceChecker, :csv_importer do
       end
 
       it 'should not enable an unrequired service' do
+        LocalLinksManager::Import::EnabledServiceChecker.new(csv_downloader).enable_services
         expect(service_2.reload.enabled).to eq(false)
       end
     end
