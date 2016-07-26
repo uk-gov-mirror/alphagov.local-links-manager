c56b2239f36e752bc05c9a673b3b3e384d6cc502
David Basalla <davidbasalla@gmail.com>
2016-07-26 16:22:09 +0000

    Add Link API Response Presenter
    We now return the required details for local authority and interaction via a presenter

---
+++ spec/presenters/link_api_response_presenter_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+describe LinkApiResponsePresenter do
+  describe '#present' do
+    let(:authority) { FactoryGirl.build(:local_authority) }
+    let(:presenter) { described_class.new(authority, link) }
+
+    context 'link is present' do
+      let(:link) { FactoryGirl.build(:link) }
+      let(:expected_response) do
+        {
+          "local_authority" => {
+            "name" => authority.name,
+            "snac" => authority.snac,
+            "tier" => authority.tier,
+            "homepage_url" => authority.homepage_url
+          },
+          "local_interaction" => {
+            "lgsl_code" => link.service.lgsl_code,
+            "lgil_code" => link.interaction.lgil_code,
+            "url" => link.url
+          }
+        }
+      end
+
+      it 'returns combined details for the local authority and local interaction' do
+        expect(presenter.present).to eq(expected_response)
+      end
+    end
+
+    context 'no link is present' do
+      let(:link) { nil }
+      let(:expected_response) do
+        {
+          "local_authority" => {
+            "name" => authority.name,
+            "snac" => authority.snac,
+            "tier" => authority.tier,
+            "homepage_url" => authority.homepage_url
+          }
+        }
+      end
+
+      it 'returns details for just the local authority' do
+        expect(presenter.present).to eq(expected_response)
+      end
+    end
+  end
+end
