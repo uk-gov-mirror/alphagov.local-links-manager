9207fd860a235b495b776a426e9ab9ccd8f874e0
Tom Gladhill <whoojemaflip@gmail.com>
2016-11-08 16:12:35 +0000

    Simplify the LinkStatusUpdater by removing unnecessary indirection.
    The `@table` and `@column` indirection were hiding the intention of this
    class. By replacing them with the class literals, we make it more
    obvious what this class's collaborators are.

--- lib/local-links-manager/check_links/link_status_updater.rb
+++ lib/local-links-manager/check_links/link_status_updater.rb
@@ -3,29 +3,27 @@ require 'local-links-manager/check_links/link_checker'
 module LocalLinksManager
   module CheckLinks
     class LinkStatusUpdater
-      attr_reader :column, :table, :url_checker
+      attr_reader :url_checker
 
       def initialize(url_checker = LinkChecker.new)
-        @table = Link
-        @column = :url
         @url_checker = url_checker
       end
 
       def update
-        links.each do |link|
-          link_response = url_checker.check_link(link)
-          update_link(link, link_response)
+        links.each do |url|
+          response = url_checker.check_link(url)
+          update_link(url, response)
         end
       end
 
     private
 
       def links
-        table.joins(:service).where(services: { enabled: true }).distinct.pluck(column)
+        Link.enabled_links.distinct.pluck(:url)
       end
 
-      def update_link(link, link_response)
-        table.where(column => link).update_all(
+      def update_link(url, link_response)
+        Link.where(url: url).update_all(
           status: link_response[:status],
           link_last_checked: link_response[:checked_at],
         )
