3029467497943849380d2c7b80e9c67b33b47966
Tom Gladhill <whoojemaflip@gmail.com>
2016-11-08 16:17:24 +0000

    Rename some class variables and methods for clarity in LinkStatusUpdater
    Small refactor to give more descriptive names to things.

--- lib/local-links-manager/check_links/link_status_updater.rb
+++ lib/local-links-manager/check_links/link_status_updater.rb
@@ -3,29 +3,29 @@ require 'local-links-manager/check_links/link_checker'
 module LocalLinksManager
   module CheckLinks
     class LinkStatusUpdater
-      attr_reader :url_checker
-
-      def initialize(url_checker = LinkChecker.new)
-        @url_checker = url_checker
+      def initialize(link_checker = LinkChecker.new)
+        @link_checker = link_checker
       end
 
       def update
-        links.each do |url|
-          response = url_checker.check_link(url)
-          update_link(url, response)
+        urls_for_enabled_services.each do |url|
+          link_checker_response = link_checker.check_link(url)
+          update_link(url, link_checker_response)
         end
       end
 
     private
 
-      def links
+      attr_reader :link_checker
+
+      def urls_for_enabled_services
         Link.enabled_links.distinct.pluck(:url)
       end
 
-      def update_link(url, link_response)
+      def update_link(url, link_checker_response)
         Link.where(url: url).update_all(
-          status: link_response[:status],
-          link_last_checked: link_response[:checked_at],
+          status: link_checker_response[:status],
+          link_last_checked: link_checker_response[:checked_at],
         )
       end
     end
