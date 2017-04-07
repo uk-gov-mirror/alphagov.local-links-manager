4539b175c8aec39398d333e2552184c4d8970e74
Isabell Long <isabell@issyl0.co.uk>
2017-04-07 14:10:29 +0000

    In a script, check if fallback links exist for each "missing" link
    - This works for each council, for each LGSL and LGIL combination.
    - With the resulting CSV (lgsl_lgil_fallback_links.csv), we run another
      script to add the new links to the database.

---
+++ lib/add_missing_fallback_links.rb
@@ -0,0 +1,13 @@
+require 'csv'
+
+CSV.foreach("lgsl_lgil_fallback_links.csv", headers: true) do |row|
+  la_slug = row[0]
+  service_slug = row[1]
+  interaction_slug = row[2]
+  link_url = row[3]
+
+  link = Link.build(local_authority_slug: la_slug, service_slug: service_slug, interaction_slug: interaction_slug)
+  link.url = link_url
+  puts "Adding link for local authority #{la_slug}, service slug #{service_slug} and interaction slug #{interaction_slug}: #{link_url}."
+  link.save!
+end
