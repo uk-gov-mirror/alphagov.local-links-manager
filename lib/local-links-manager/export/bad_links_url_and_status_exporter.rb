105378cb7101d9a93adabdf5185f16b06ec5751d
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2017-07-06 10:39:38 +0100

    Update bad links exporter to include problem summary

--- lib/local-links-manager/export/bad_links_url_and_status_exporter.rb
+++ lib/local-links-manager/export/bad_links_url_and_status_exporter.rb
@@ -1,14 +1,15 @@
-require 'csv'
+require "csv"
 
 module LocalLinksManager
   module Export
     class BadLinksUrlAndStatusExporter
-      HEADINGS = %w(url link_errors link_warnings).freeze
+      HEADINGS = %w(url status).freeze
+
       def self.local_authority_bad_homepage_url_and_status_csv
         CSV.generate do |csv|
           csv << HEADINGS
-          LocalAuthority.where.not(status: "ok").pluck(:homepage_url, :link_errors, :link_warnings).each do |la|
-            csv << [la[0], la[1].join(','), la[2].join(',')]
+          LocalAuthority.where(status: "broken").distinct.pluck(:homepage_url, :problem_summary).each do |row|
+            csv << row
           end
         end
       end
@@ -16,9 +17,8 @@ module LocalLinksManager
       def self.bad_links_url_and_status_csv
         CSV.generate do |csv|
           csv << HEADINGS
-          Link.enabled_links.where.not(status: "ok").group_by(&:url).each do |links|
-            link = links.last.pluck(:url, :link_errors, :link_warnings).first
-            csv << [link[0], link[1].join(','), link[2].join(',')]
+          Link.enabled_links.currently_broken.distinct.pluck(:url, :problem_summary).each do |row|
+            csv << row
           end
         end
       end
--- spec/lib/local-links-manager/export/bad_links_url_and_status_exporter_spec.rb
+++ spec/lib/local-links-manager/export/bad_links_url_and_status_exporter_spec.rb
@@ -3,43 +3,42 @@ require 'local-links-manager/export/bad_links_url_and_status_exporter'
 
 describe LocalLinksManager::Export::BadLinksUrlAndStatusExporter do
   let(:exporter) { LocalLinksManager::Export::BadLinksUrlAndStatusExporter }
-  let(:errors) { ["Received 404 response from the server."] }
-  let(:warnings) { ["Received 204 response from the server."] }
+  let(:error_problem_summary) { "Received 404 response from the server." }
+  let(:warning_problem_summary) { "Received 204 response from the server." }
 
   describe ".local_authority_bad_homepage_url_and_status_csv" do
     before do
-      create(:local_authority, homepage_url: "http://www.hogsmeade.gov.uk", status: "broken", link_errors: errors)
-      create(:local_authority, homepage_url: "http://www.littlehangleton.gov.uk", status: "caution", link_warnings: warnings)
+      create(:local_authority, homepage_url: "http://www.hogsmeade.gov.uk", status: "broken", problem_summary: error_problem_summary)
+      create(:local_authority, homepage_url: "http://www.littlehangleton.gov.uk", status: "caution", problem_summary: warning_problem_summary)
       create(:local_authority, homepage_url: "http://www.diagonalley.gov.uk", status: "ok")
     end
 
-    it "returns the URL and errors and warnings for each local authority with a non-200 homepage" do
-      expect(exporter.local_authority_bad_homepage_url_and_status_csv).to include("url,link_errors,link_warnings\n")
-      expect(exporter.local_authority_bad_homepage_url_and_status_csv).to include("http://www.hogsmeade.gov.uk,Received 404 response from the server.,\"\"\n")
-      expect(exporter.local_authority_bad_homepage_url_and_status_csv).to include("http://www.littlehangleton.gov.uk,\"\",Received 204 response from the server.\n")
+    it "returns the URL and errors for each local authority with a non-200 homepage" do
+      expect(exporter.local_authority_bad_homepage_url_and_status_csv).to include("url,status\n")
+      expect(exporter.local_authority_bad_homepage_url_and_status_csv).to include("http://www.hogsmeade.gov.uk,Received 404 response from the server.\n")
     end
 
     it "does not return the URL and errors and warnings for a local authority with a 200 homepage" do
-      expect(exporter.local_authority_bad_homepage_url_and_status_csv).not_to include
-      "http://www.diagonalley.gov.uk,\"\",\"\"\n"
+      expect(exporter.local_authority_bad_homepage_url_and_status_csv).not_to include("http://www.diagonalley.gov.uk")
+      expect(exporter.local_authority_bad_homepage_url_and_status_csv).not_to include("http://www.littlehangleton.gov.uk")
     end
   end
 
   describe ".bad_links_url_and_status_csv" do
     before do
-      create(:link, url: "http://www.hogsmeade.gov.uk/apply-to-hogwarts", status: "broken", link_errors: errors)
-      create(:link, url: "http://www.littlehangleton.gov.uk/broomstick-permits", status: "caution", link_warnings: warnings)
+      create(:link, url: "http://www.hogsmeade.gov.uk/apply-to-hogwarts", status: "broken", problem_summary: error_problem_summary)
+      create(:link, url: "http://www.littlehangleton.gov.uk/broomstick-permits", status: "caution", problem_summary: warning_problem_summary)
       create(:link, url: "http://www.diagonalley.gov.uk/report-owl-fouling", status: "ok")
     end
 
     it "returns the URL and errors and warnings for each non-200 link" do
-      expect(exporter.bad_links_url_and_status_csv).to include("url,link_errors,link_warnings\n")
-      expect(exporter.bad_links_url_and_status_csv).to include("http://www.hogsmeade.gov.uk/apply-to-hogwarts,Received 404 response from the server.,\"\"\n")
-      expect(exporter.bad_links_url_and_status_csv).to include("http://www.littlehangleton.gov.uk/broomstick-permits,\"\",Received 204 response from the server.\n")
+      expect(exporter.bad_links_url_and_status_csv).to include("url,status\n")
+      expect(exporter.bad_links_url_and_status_csv).to include("http://www.hogsmeade.gov.uk/apply-to-hogwarts,Received 404 response from the server.\n")
     end
 
-    it "does not return the URL and errors and warnings for a 200 link" do
-      expect(exporter.bad_links_url_and_status_csv).not_to include("http://www.diagonalley.gov.uk/report-owl-fouling,\"\",\"\"\n")
+    it "does not return the URL and status for a 200 link" do
+      expect(exporter.bad_links_url_and_status_csv).not_to include("http://www.diagonalley.gov.uk/report-owl-fouling")
+      expect(exporter.bad_links_url_and_status_csv).not_to include("http://www.littlehangleton.gov.uk/broomstick-permits")
     end
   end
 end
