5fa6fa1d8868150629387f51e561f2b43bfd1423
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2017-07-11 17:46:31 +0100

    Change link checking strategy
    If we submit all the links for a local authority at once, we have noticed that
    several authorities fail link checking for lots of their links.  I guess that
    this is because they haven't warmed up sufficiently at 2AM when the link
    checker runs.  We run 4 threads on the sidekiq worker at present and it's
    possible that we are causing our own bottleneck.
    
    I propose a different strategy where we check links by service instead.  This
    spaces out the requests to local authority websites a bit which allows them
    extra time to warm up.
    
    I've also changed the order so that the least significant (by GA click count)
    is requested first.  I'm hoping that this will contribute to a more accurate
    link checker status for the high value links in local links manager which is
    what we're after.
    
    We check the homepages last of all as it's most important that they are correct.
    
    I've also added a rake task allowing us to recheck a whole council, as
    sometimes they are down for maintenance overnight. There is nothing that our
    admins can do, so we should be able to easily recheck.

--- lib/local-links-manager/check_links/link_status_requester.rb
+++ lib/local-links-manager/check_links/link_status_requester.rb
@@ -6,16 +6,35 @@ module LocalLinksManager
       delegate :url_helpers, to: "Rails.application.routes"
 
       def call
-        LocalAuthority.all.each do |local_authority|
-          link_checker_api.create_batch(
-            urls_for_local_authority(local_authority),
-            webhook_uri: webhook_uri,
-            webhook_secret_token: webhook_secret_token
-          )
+        Service.enabled.each do |service|
+          check_urls service.links.order(analytics: :asc).map(&:url).uniq
         end
+
+        check_urls homepage_urls
+      end
+
+      def check_authority_urls(authority_slug)
+        check_urls urls_for_authority(authority_slug).uniq
       end
 
     private
+
+      def urls_for_authority(authority_slug)
+        local_authority = LocalAuthority.find_by(slug: authority_slug)
+        local_authority.links.map(&:url) << local_authority.homepage_url
+      end
+
+      def homepage_urls
+        LocalAuthority.all.map(&:homepage_url)
+      end
+
+      def check_urls(urls)
+        link_checker_api.create_batch(
+          urls,
+          webhook_uri: webhook_uri,
+          webhook_secret_token: webhook_secret_token
+        )
+      end
 
       def webhook_uri
         Plek.find("local-links-manager") + url_helpers.link_checker_webhook_path
@@ -23,11 +42,6 @@ module LocalLinksManager
 
       def webhook_secret_token
         Rails.application.secrets.link_checker_api_secret_token
-      end
-
-      def urls_for_local_authority(local_authority)
-        (local_authority.provided_service_links.map(&:url) +
-          [local_authority.homepage_url]).uniq
       end
 
       def link_checker_api
