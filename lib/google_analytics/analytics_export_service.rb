0e0ddd946804d3f0eeadc82f8216725ee4073d38
Elena Tanasoiu <elena.tanasoiu@gmail.com>
2017-11-02 16:00:04 +0000

    Add GA export service for bad links
    This will export a list of bad links that is obtained from the BadLinksUrlandStatusExporter.
    The list is in CSV format and contains the page url and status, and indicates why a link is currently failing.
    In this case "bad" means the link has a non-200 status.
    
    In order to be able to upload files, a service account was created called "BadLinksExporter". This account has permission to upload to GA,
    which is granted through a json file that you can download from the service account. The information you will need to extract from this file
    will be the `client_email` and the `private_key`. You will then add these in a yml file which is described in detail below.
    
    Running the GA code locally can be done by creating a file at `config/local_env.yml` which should contain:
    - a `GOOGLE_CLIENT_EMAIL` (this is extracted from the JSON file containing the credentials for your service account)
    - a `GOOGLE_PRIVATE_KEY` (this is extracted from the JSON file containing the credentials for your service account)
    - a `GOOGLE_EXPORT_ACCOUNT_ID` (this is obtained from the GA account domain you are trying to upload to. We use the account id for `www.gov.uk`)
    - a `GOOGLE_EXPORT_TRACKER_ID` (this is the web property UA string associated with the upload. Ex: `UA-XXXXXXXXX-X`)
    - a `GOOGLE_EXPORT_CUSTOM_DATA_IMPORT_SOURCE_ID` (this is obtained from the custom data import setup in GA. Ex: `abcdeFGHijk12_AB12cd`)
    These values are set as environment variables.
    
    Once authentication has been obtained, the end result will be an uploaded CSV file to GA via the Analytics API.

---
+++ lib/google_analytics/analytics_export_service.rb
@@ -0,0 +1,35 @@
+require 'google/apis/analytics_v3'
+require 'googleauth'
+
+module GoogleAnalytics
+  class AnalyticsExportService
+    include Google::Apis::AnalyticsV3
+    include Google::Auth
+
+    attr_accessor :service
+
+    def build
+      @service = AnalyticsService.new
+      @service.authorization ||= ServiceAccountCredentials.make_creds(
+        json_key_io: File.open(File.join(Rails.root, 'config', 'analytics_exporter_client_secrets.json')),
+        scope: 'https://www.googleapis.com/auth/analytics.edit')
+      @service
+    end
+
+    def export_bad_links(data)
+      begin
+        response = ''
+        Tempfile.create('bad_links.csv', Dir.pwd) do |file|
+          file.write(data)
+          response = service.upload_data(ENV['GOOGLE_EXPORT_ACCOUNT_ID'],
+                                         ENV['GOOGLE_EXPORT_CUSTOM_DATA_IMPORT_SOURCE_ID'],
+                                         ENV['GOOGLE_EXPORT_TRACKER_ID'],
+                                         fields: "accountId,customDataSourceId,errors,id,kind,status,uploadTime",
+                                         upload_source: file.path,
+                                         content_type: 'application/octet-stream')
+        end
+      end
+      response
+    end
+  end
+end
