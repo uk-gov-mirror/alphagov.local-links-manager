2ab0548e37d32a36968130e884ee2b409a35c547
Elena Tanasoiu <elena.tanasoiu@gmail.com>
2017-11-02 15:52:50 +0000

    Adds version of bad links export with GA headers
    This will be used to create a file that will be exported to GA using the Google Analytics API.
    The file will contain a list of bad links with the headers expected by Google Analytics instead of the usual ones.

--- lib/local-links-manager/export/bad_links_url_and_status_exporter.rb
+++ lib/local-links-manager/export/bad_links_url_and_status_exporter.rb
@@ -4,6 +4,7 @@ module LocalLinksManager
   module Export
     class BadLinksUrlAndStatusExporter
       HEADINGS = %w(url status).freeze
+      GA_HEADINGS = %w(ga:dimension36 ga:dimension37).freeze
 
       def self.local_authority_bad_homepage_url_and_status_csv
         CSV.generate do |csv|
@@ -14,9 +15,9 @@ module LocalLinksManager
         end
       end
 
-      def self.bad_links_url_and_status_csv
+      def self.bad_links_url_and_status_csv(with_ga_headings: false)
         CSV.generate do |csv|
-          csv << HEADINGS
+          csv << (with_ga_headings ? GA_HEADINGS : HEADINGS)
           Link.enabled_links.currently_broken.distinct.pluck(:url, :problem_summary).each do |row|
             csv << row
           end
