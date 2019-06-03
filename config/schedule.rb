b58b8fedd5f743852ed0df7cae31f352b5f5a292
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2019-06-03 14:58:17 +0100

    Import services every night instead of once a month

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,13 +3,13 @@ env :PATH, '/usr/local/bin:/usr/bin:/bin'
 set :output, {:error => 'log/cron.error.log', :standard => 'log/cron.log'}
 job_type :rake, 'cd :path && /usr/local/bin/govuk_setenv local-links-manager bundle exec rake :task :output'
 
-every :day, at: '2am' do
-  rake 'check-links'
+# Run the rake task to import all services and interactions into Local Links Manager
+every :day, at: '1am' do
+  rake 'import:service_interactions:import_all'
 end
 
-# Run the rake task to import all services and interactions into Local Links Manager on the 1st of each month
-every :month, on: '1st' do
-  rake 'import:service_interactions:import_all'
+every :day, at: '2am' do
+  rake 'check-links'
 end
 
 # Run the rake task to export data to CSV for data.gov.uk.
