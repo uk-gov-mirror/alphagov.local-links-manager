c29d7f88e610b46a373e9f3306e1bd0910a41614
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2020-04-17 09:21:26 +0100

    Avoid using instance variables

--- app/presenters/link_api_response_presenter.rb
+++ app/presenters/link_api_response_presenter.rb
@@ -5,32 +5,32 @@ class LinkApiResponsePresenter
   end
 
   def present
-    if @link
-      local_authority_details.merge(link_details)
-    else
-      local_authority_details
-    end
+    local_authority_details.merge(link_details)
   end
 
 private
 
+  attr_reader :authority, :link
+
   def local_authority_details
     {
       "local_authority" => {
-        "name" => @authority.name,
-        "snac" => @authority.snac,
-        "tier" => @authority.tier,
-        "homepage_url" => @authority.homepage_url,
+        "name" => authority.name,
+        "snac" => authority.snac,
+        "tier" => authority.tier,
+        "homepage_url" => authority.homepage_url,
       },
     }
   end
 
   def link_details
+    return {} unless link
+
     {
       "local_interaction" => {
-        "lgsl_code" => @link.service.lgsl_code,
-        "lgil_code" => @link.interaction.lgil_code,
-        "url" => @link.url,
+        "lgsl_code" => link.service.lgsl_code,
+        "lgil_code" => link.interaction.lgil_code,
+        "url" => link.url,
       },
     }
   end
