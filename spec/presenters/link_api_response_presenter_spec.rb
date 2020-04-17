82d1c8e4ff965f315efa2f9f82bb3eafdb52cb5a
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2020-04-17 09:26:40 +0100

    Refactor LinkApiResponsePresenter tests

--- spec/presenters/link_api_response_presenter_spec.rb
+++ spec/presenters/link_api_response_presenter_spec.rb
@@ -1,25 +1,32 @@
 describe LinkApiResponsePresenter do
   describe "#present" do
     let(:authority) { build(:district_council) }
-    let(:presenter) { described_class.new(authority, link) }
+    subject(:presenter) { described_class.new(authority, link) }
+
+    def presented_local_authority(authority)
+      {
+        "local_authority" => {
+          "name" => authority.name,
+          "snac" => authority.snac,
+          "tier" => authority.tier,
+          "homepage_url" => authority.homepage_url,
+        },
+      }
+    end
+
+    def presented_link(link)
+      {
+        "local_interaction" => {
+          "lgsl_code" => link.service.lgsl_code,
+          "lgil_code" => link.interaction.lgil_code,
+          "url" => link.url,
+        },
+      }
+    end
 
     context "link is present" do
-      let(:link) { create(:link) }
-      let(:expected_response) do
-        {
-          "local_authority" => {
-            "name" => authority.name,
-            "snac" => authority.snac,
-            "tier" => "district",
-            "homepage_url" => authority.homepage_url,
-          },
-          "local_interaction" => {
-            "lgsl_code" => link.service.lgsl_code,
-            "lgil_code" => link.interaction.lgil_code,
-            "url" => link.url,
-          },
-        }
-      end
+      let(:link) { create(:link, local_authority: authority) }
+      let(:expected_response) { presented_local_authority(authority).merge(presented_link(link)) }
 
       it "returns combined details for the local authority and local interaction" do
         expect(presenter.present).to eq(expected_response)
@@ -28,16 +35,7 @@ describe LinkApiResponsePresenter do
 
     context "no link is present" do
       let(:link) { nil }
-      let(:expected_response) do
-        {
-          "local_authority" => {
-            "name" => authority.name,
-            "snac" => authority.snac,
-            "tier" => "district",
-            "homepage_url" => authority.homepage_url,
-          },
-        }
-      end
+      let(:expected_response) { presented_local_authority(authority) }
 
       it "returns details for just the local authority" do
         expect(presenter.present).to eq(expected_response)
