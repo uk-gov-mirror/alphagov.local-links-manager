9eeb55538b720e589865373fccbb444c98268f0c
Huw Diprose <mail@huwdiprose.co.uk>
2020-05-01 13:52:53 +0100

    Add migration for service LGSL code: 1287 - Covid-19
    Update schema
    
    We've had some real difficulties getting a dev version of local links manager with proper data, so i've fudged migrating this properly and updated the string here :grimace: as the migration doesn't actually create any schema changes that should be ok..

---
+++ db/migrate/20200501124957_enable_service_1287.rb
@@ -0,0 +1,18 @@
+class EnableService1287 < ActiveRecord::Migration[6.0]
+  def up
+    service = Service.find_by(lgsl_code: 1287)
+    interaction = Interaction.find_by(lgil_code: 8)
+
+    service.update!(enabled: true)
+    ServiceTier.create_tiers([Tier.district, Tier.unitary, Tier.county], service)
+
+    service_interaction = ServiceInteraction.find_by(service: service, interaction: interaction)
+
+    if service_interaction
+      service_interaction.update!(live: true)
+      puts "Successfully enabled service 1287"
+    else
+      raise "Service 1287 has not been imported from ESD"
+    end
+  end
+end
