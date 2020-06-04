9b31b8aa6e7f7308942c0b5961dafa7c28b98de2
callumknights <callum.knights@digital.cabinet-office.gov.uk>
2020-06-04 17:16:45 +0100

    Rubocop corrections.

--- db/migrate/20200604142453_add_default_to_analytics.rb
+++ db/migrate/20200604142453_add_default_to_analytics.rb
@@ -1,8 +1,9 @@
 class AddDefaultToAnalytics < ActiveRecord::Migration[6.0]
   def up
-    change_column :links, :analytics, :integer, :default => 0
+    change_column :links, :analytics, :integer, default: 0
   end
+
   def down
-    change_column :links, :analytics, :integer, :default => nil
+    change_column :links, :analytics, :integer, default: nil
   end
 end
