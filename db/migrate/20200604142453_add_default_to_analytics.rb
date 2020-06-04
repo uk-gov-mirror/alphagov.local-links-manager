9c4a24a84cfac521186824627a435df05f6b6e4c
callumknights <callum.knights@digital.cabinet-office.gov.uk>
2020-06-04 16:54:07 +0100

    Add a zero default to analytics column
    Will be first of two migrations to hopefully correct the ordering behaviour.

---
+++ db/migrate/20200604142453_add_default_to_analytics.rb
@@ -0,0 +1,8 @@
+class AddDefaultToAnalytics < ActiveRecord::Migration[6.0]
+  def up
+    change_column :links, :analytics, :integer, :default => 0
+  end
+  def down
+    change_column :links, :analytics, :integer, :default => nil
+  end
+end
