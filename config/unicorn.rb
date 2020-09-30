7347a1d18e1541676b96b96dd516864c855f0b28
Andrew White <andrew.white@unboxed.co>
2020-09-30 11:47:37 +0100

    Preload application for unicorn before it forks
    To take maximum advantage of the copy-on-write memory feature the app
    should be preloaded first before unicorn forks any workers.
    
    The before/after_fork hooks are based on the unicorn config for whitehall.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,3 +1,19 @@
 require "govuk_app_config/govuk_unicorn"
 GovukUnicorn.configure(self)
+
 working_directory File.dirname(File.dirname(__FILE__))
+
+# Preload the entire app
+preload_app true
+
+before_fork do |_server, _worker|
+  # The following is highly recomended for Rails + "preload_app true"
+  # as there's no need for the master process to hold a connection.
+  defined?(ActiveRecord::Base) &&
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |_server, _worker|
+  defined?(ActiveRecord::Base) &&
+    ActiveRecord::Base.establish_connection
+end
