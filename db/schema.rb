01fdcf99dffb91df35b05119afe95989fae646ca
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2023-05-04 13:00:45 +0100

    Allow null snac in database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_07_103144) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_04_115751) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,7 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_103144) do
     t.string "homepage_url"
     t.string "name", null: false
     t.string "slug", null: false
-    t.string "snac", null: false
+    t.string "snac"
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
     t.string "status"
