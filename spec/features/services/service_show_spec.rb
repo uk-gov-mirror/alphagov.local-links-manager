b7655d3b8929fb7b0e5e5f26b2c7e09cb2ad7c04
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-02-06 15:17:17 +0000

    Update tests for Service show/index

--- spec/features/services/service_show_spec.rb
+++ spec/features/services/service_show_spec.rb
@@ -25,7 +25,7 @@ feature "The services show page" do
   end
 
   it "has a breadcrumb trail" do
-    expect(page).to have_selector(".breadcrumb")
+    expect(page).to have_selector(".govuk-breadcrumbs__list")
   end
 
   it "displays the name of the service" do
@@ -38,83 +38,29 @@ feature "The services show page" do
     expect(page).to have_content(@council_z.name)
   end
 
-  it "lists the councils in alphabetic order" do
-    expect(@council_a.name).to appear_before(@council_z.name)
-  end
-
-  it "shows a count of the number of broken links" do
-    within("thead") do
-      expect(page).to have_content "2 links"
-    end
-  end
-
   it "displays a filter box" do
-    expect(page).to have_selector(".filter-control")
-  end
-
-  it "has navigation tabs" do
-    expect(page).to have_selector(".link-nav")
-    within(".link-nav") do
-      expect(page).to have_link "Broken links"
-      expect(page).to have_link "All links"
-    end
-  end
-
-  describe "broken links" do
-    before do
-      click_link "Broken links"
-    end
-
-    it "shows non-200 status links" do
-      expect(page).to have_link @link2.url
-    end
-
-    it "doesn't show 200 status links" do
-      expect(page).not_to have_link @link1.url
-    end
+    expect(page).to have_selector(".js-gem-c-table__filter")
   end
 
   describe "for each local authority" do
     it "the Local Authority name is linked to the council specific page" do
-      for_local_authority(@council_a) do
-        expect(page).to have_link @council_a.name, href: local_authority_path(@council_a)
-      end
+      expect(page).to have_link @council_a.name, href: local_authority_path(@council_a)
     end
 
-    it "the Service name is linked to the service page for that council" do
-      for_local_authority_interactions(@council_a, @link1.interaction) do
-        expect(page).to have_text @service.label
-      end
+    it "an edit link points to the edit form for that council/interaction" do
+      expect(page).to have_link "Edit", href: edit_link_path(@council_a, @service, @link1.service_interaction.interaction)
     end
 
-    it "the Interaction name is printed" do
-      for_local_authority_interactions(@council_a, @link1.interaction) do
-        expect(page).to have_text @link1.interaction.label
-      end
+    it "the Service name is printed" do
+      expect(page).to have_text @service.label
     end
 
     it "shows the link status as 'Good' when the status is 200" do
-      for_local_authority_interactions(@council_a, @link1.interaction) do
-        expect(page).to have_text "Good"
-      end
+      expect(page).to have_text "#{@council_a.name} Good"
     end
 
     it "shows the link status as 'Broken: 404 error (page not found)' when the status is 404" do
-      for_local_authority_interactions(@council_z, @link2.interaction) do
-        expect(page).to have_text "Broken: 404 error (page not found)"
-      end
-    end
-
-    it "shows the link last checked details" do
-      for_local_authority_interactions(@council_a, @link1.interaction) do
-        expect(page).to have_text "Link not checked"
-      end
-    end
-
-    it "should have a link to Edit Link" do
-      for_local_authority_interactions(@council_a, @link1.interaction) do
-        expect(page).to have_link "Edit link", href: edit_link_path(@council_a, @service, @link1.interaction)
-      end
+      expect(page).to have_text "#{@council_z.name} Broken: 404 error (page not found)"
     end
   end
 
@@ -122,18 +68,6 @@ feature "The services show page" do
     it "returns a 404" do
       visit service_path("bed-pans")
       expect(page.status_code).to eq(404)
-    end
-  end
-
-  def for_local_authority(council, &block)
-    within(".header-row[data-local-authority-id=\"#{council.id}\"]", &block)
-  end
-
-  def for_local_authority_interactions(council, interaction = nil, &block)
-    if interaction
-      within("[data-local-authority-id=\"#{council.id}\"][data-service-id=\"#{@service.id}\"][data-interaction-id=\"#{interaction.id}\"]", &block)
-    else
-      within("[data-local-authority-id=\"#{council.id}\"][data-service-id=\"#{@service.id}\"]", &block)
     end
   end
 end
