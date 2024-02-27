88342e491025adc2d8be4af6ad10c6133383607b
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-02-27 15:20:29 +0000

    Add unofficial summary item to service presenter

--- app/presenters/service_presenter.rb
+++ app/presenters/service_presenter.rb
@@ -4,10 +4,11 @@ class ServicePresenter < SimpleDelegator
       si.govuk_title ? view_context.link_to(si.govuk_title, "#{Plek.website_root}/#{si.govuk_slug}", class: "govuk-link") : nil
     end
 
-    summary_items = [
-      { field: "Local Government Service List (LGSL) Code", value: lgsl_code },
-      { field: "GOV.UK Pages", value: govuk_links.compact.any? ? govuk_links.compact.join("</br>").html_safe : "Not used on GOV.UK" },
-    ]
+    summary_items = []
+    summary_items <<  { field: "Local Government Service List (LGSL) Code", value: lgsl_code }
+    summary_items <<  { field: "GOV.UK Pages", value: govuk_links.compact.any? ? govuk_links.compact.join("</br>").html_safe : "Not used on GOV.UK" }
+
+    summary_items << { field: "Unofficial", value: "Not specified by <a href=\"https://standards.esd.org.uk/?uri=list%2FenglishAndWelshServices\" class=\"govuk-link\">esd.org.uk</a>".html_safe } if unofficial
 
     { items: summary_items }
   end
