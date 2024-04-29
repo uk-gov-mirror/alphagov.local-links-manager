51e06b597b89b7f93bbce42ab2833083dfad021e
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-04-29 14:33:37 +0100

    Add test for export:links:s3

---
+++ spec/lib/tasks/export/link_exporter_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.describe "Export tasks" do
+  describe "export:links:s3" do
+    it "should write the links to S3" do
+      service = create(:service, :all_tiers, lgsl_code: 1)
+      service_interaction = create(:service_interaction, service:)
+      link = create(:link, service_interaction:)
+
+      la = LocalAuthority.last
+      interaction = service_interaction.interaction
+      expected_body = <<~EXPECTED_BODY_TEXT
+        Authority Name,GSS,Description,LGSL,LGIL,URL,Supported by GOV.UK
+        #{la.name},#{la.gss},#{service.label}: #{interaction.label},#{service.lgsl_code},#{interaction.lgil_code},#{link.url},true
+      EXPECTED_BODY_TEXT
+
+      s3 = double
+      allow(Aws::S3::Client).to receive(:new).and_return(s3)
+      expect(s3).to receive(:put_object).with({
+        body: expected_body,
+        bucket: nil,
+        key: "data/local-links-manager/links_to_services_provided_by_local_authorities.csv",
+      })
+
+      Rake::Task["export:links:s3"].execute
+    end
+  end
+end
