7b7c27e464f289e549eb09fec1b39f5572ea259f
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-12-03 14:19:59 +0000

    Break config.hosts DO NOT MERGE

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,6 @@ Rails.application.configure do
   # Enable DNS rebinding protection and other `Host` header attacks.
   config.hosts = [
     /local-links-manager\..*\.gov.uk$/,
-    "local-links-manager",
   ]
 
   # Skip DNS rebinding protection for the default health check endpoint.
