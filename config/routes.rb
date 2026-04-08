c62dfb174153b672eeb2b00cca0b1a3253979635
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2026-04-08 11:16:32 +0100

    Add RailsCache to the /healthcheck/ready endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "/healthcheck/live", to: proc { [200, {}, %w[OK]] }
   get "/healthcheck/ready", to: GovukHealthcheck.rack_response(
     GovukHealthcheck::ActiveRecord,
+    GovukHealthcheck::RailsCache,
     GovukHealthcheck::Redis,
   )
 
